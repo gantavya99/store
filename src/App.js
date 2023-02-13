c8eca6043f5881c1b74b6c3b656b0df15d126721
Gantavya Saraswat <gantavya99@gmail.com>
2023-02-13 15:26:38 +0530

    Set up Redux Store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { Component }  from 'react';
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+import store from './store/store';
+import { Provider } from 'react-redux';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Provider store={store}>
+     <BrowserRouter>
+     <Navbar />
+     <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Cart" element={<Cart />}></Route>
+     </Routes>
+     </BrowserRouter>
+     </Provider>
     </div>
   );
 }
